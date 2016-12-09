abef34ca025c21bc41025f82d202351f07924f52
Ken Yu <kyu_115s@hotmail.com>
2016-12-09 23:23:38 +0800

    Add test classes to an additional packaged JAR.

--- sbin/test-data-managing-app.sh
+++ sbin/test-data-managing-app.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -classpath bin/las-vpe-platform-0.0.1-full.jar org.cripac.isee.vpe.alg.PedestrianAttrRecogAppTest
+java -classpath bin/las-vpe-platform-0.0.1-full.jar:bin/las-vpe-platform-0.0.1-tests.jar org.cripac.isee.vpe.data.DataManagingAppTest
