8480ec0b9e0cca35b763b39417e882c26ffa5bfe
Ken Yu <kyu_115s@hotmail.com>
2017-03-27 17:11:28 +0800

    Add building script for Caffe.

--- sbin/build-native-libs.sh
+++ sbin/build-native-libs.sh
@@ -55,4 +55,25 @@ cp -Rpu lib/libbasic_pedestrian_tracker.so ${PROJECT_PATH}/lib/linux || :
 cp -Rpu lib/jni/libbasic_pedestrian_tracker_jni.so ${PROJECT_PATH}/lib/linux || :
 ##################################################
 
+##################################################
+# Build Caffe
+##################################################
+echo "Building Caffe..."
+##################################################
+cd ${NATIVE_SRC}/caffe
+if [ $? -ne 0 ]
+then
+  exit $?
+fi
+##################################################
+make clean
+make -j 16
+if [ $? -ne 0 ]
+then
+  exit $?
+fi
+##################################################
+cp -Rpu .build_release/lib/libcaffe.so* ${PROJECT_PATH}/lib/linux || :
+##################################################
+
 echo "Successfully finished building native libraries!"
