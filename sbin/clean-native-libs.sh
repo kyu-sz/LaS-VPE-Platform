69e5fd6d89015b1fc75ba3b30f4323931883a00e
Ken Yu <kyu_115s@hotmail.com>
2017-04-10 13:53:13 +0800

    Minor fixes.

--- sbin/clean-native-libs.sh
+++ sbin/clean-native-libs.sh
@@ -4,16 +4,16 @@ projectpath=$(cd `dirname "${BASH_SOURCE[0]}"`/..; pwd)
 # Clean ISEE-Basic-Pedestrian-Tracker
 rm ${projectpath}/lib/x64/libbasic_pedestrian_tracker.so
 rm ${projectpath}/lib/x64/libbasic_pedestrian_tracker_jni.so
-cd ${projectpath}/ISEE-Basic-Pedestrian-Tracker
+cd ${projectpath}/src/native/ISEE-Basic-Pedestrian-Tracker/Release
 make clean
 
 # Clean Caffe
 rm ${projectpath}/lib/x64/libcaffe.so*
-cd ${projectpath}/caffe
+cd ${projectpath}/src/native/caffe/
 make clean
 
 # Clean DeepMAR_deploy
 rm ${projectpath}/lib/x64/libDeepMAR.so
 rm ${projectpath}/lib/x64/libDeepMAR_caffe_jni.so
-cd ${projectpath}/DeepMAR_deploy
+cd ${projectpath}/src/native/DeepMAR_deploy/Release
 make clean
