c9221fc693585f117d9bd4260a6929ea359097b1
Ken Yu <kyu_115s@hotmail.com>
2017-04-24 18:05:15 +0800

    Add speed test for recognizer only.

--- src/main/java/org/cripac/isee/vpe/alg/pedestrian/attr/PedestrianAttrRecogApp.java
+++ src/main/java/org/cripac/isee/vpe/alg/pedestrian/attr/PedestrianAttrRecogApp.java
@@ -199,6 +199,7 @@ public class PedestrianAttrRecogApp extends SparkStreamingApp {
                     .foreachRDD(rdd -> rdd.glom().foreach(kvList -> {
                         Logger logger = loggerSingleton.getInst();
                         long startTime = System.currentTimeMillis();
+                        final long[] recognizerCostTime = {0};
                         kvList.forEach(kv -> {
                             try {
                                 final UUID taskID = kv._1();
@@ -209,7 +210,10 @@ public class PedestrianAttrRecogApp extends SparkStreamingApp {
                                 // Recognize attributes robustly.
                                 final Attributes attr = new RobustExecutor<>((Function<TrackletOrURL, Attributes>) tou -> {
                                     final Tracklet t = tou.getTracklet();
+                                    long recogStartTime = System.currentTimeMillis();
                                     final Attributes a = recognizerSingleton.getInst().recognize(t);
+                                    long recogEndTime = System.currentTimeMillis();
+                                    recognizerCostTime[0] += (recogEndTime - recogStartTime) / t.getSamples().size();
                                     a.trackletID = t.id;
                                     return a;
                                 }).execute((TrackletOrURL) taskData.predecessorRes);
@@ -230,7 +234,8 @@ public class PedestrianAttrRecogApp extends SparkStreamingApp {
                         });
                         if (kvList.size() > 0) {
                             long endTime = System.currentTimeMillis();
-                            logger.info("Average cost time: " + ((endTime - startTime) / kvList.size()) + "ms");
+                            logger.info("Recognizer speed=" + (recognizerCostTime[0] / kvList.size()) + "ms per sample");
+                            logger.info("Overall speed=" + ((endTime - startTime) / kvList.size()) + "ms per tracklet");
                         }
                     }));
         }
