8bec3609966332a7b5056e35202be0eda0df3765
JackLi31742 <317426838@qq.com>
2017-11-12 17:06:41 +0800

    未测试版

--- src/main/java/org/cripac/isee/alg/pedestrian/attr/DeepMARCaffe2NativeMul.java
+++ src/main/java/org/cripac/isee/alg/pedestrian/attr/DeepMARCaffe2NativeMul.java
@@ -1,6 +1,8 @@
 package org.cripac.isee.alg.pedestrian.attr;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.cripac.isee.alg.pedestrian.tracking.Tracklet;
 import org.cripac.isee.alg.pedestrian.tracking.Tracklet.BoundingBox;
@@ -10,13 +12,16 @@ import org.cripac.isee.vpe.util.logging.Logger;
 
 public class DeepMARCaffe2NativeMul  implements DeepMARCaffe2, BatchRecognizer{
 	
+	List<DeepMARCaffe2Native> deepMARCaffe2NativeList=new ArrayList<>();
+	
 	public DeepMARCaffe2NativeMul(AppPropertyCenter propCenter,Singleton<Logger> loggerSingleton){
 		Collection<Integer> gpus=propCenter.gpus;
 		gpus.forEach(f->{
 			System.out.println("当前gpu是："+f);
 		
 			try {
-				new DeepMARCaffe2Native(String.valueOf(f), loggerSingleton.getInst());
+				DeepMARCaffe2Native deepMARCaffe2Native=new DeepMARCaffe2Native(String.valueOf(f), loggerSingleton.getInst());
+				deepMARCaffe2NativeList.add(deepMARCaffe2Native);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
